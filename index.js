b9881c69f035961fac58a8a5eca0a9f0c1269ab6
Akshat Dwivedi <akshatdwi755@gmail.com>
2024-10-14 17:52:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function(){
     console.log("button clicked !")
-}
-)
+})
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = []
+const  inputEl = document.getElementById("input-el")
